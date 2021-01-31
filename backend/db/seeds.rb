158666bc275070b3d5fde4329714effb540ffaec
CoderJay06 <jaycru06@yahoo.com>
2021-01-30 23:41:40 -0500

    Add User seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'jay@flatiron.com', username: 'jay06', password: 'j123')
+User.create(email: 'bob@email.com', username: 'bob01', password: 'bob123')
+User.create(email: 'mike@flatiron.com', username: 'mike01', password: 'mik123')
+User.create(email: 'cindy@mail.com', username: 'cindy02', password: 'cind123')
+User.create(email: 'sarah@mail.com', username: 'sarah05', password: 'sara123')
+User.create(email: 'avi@example.com', username: 'avi04', password: 'av123')
