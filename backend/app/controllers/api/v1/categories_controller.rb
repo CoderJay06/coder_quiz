b6f05fb46ab03bcdcc1fd4b68d6494863d145296
CoderJay06 <jaycru06@yahoo.com>
2021-02-03 14:51:42 -0500

    Add actions to categories controller

--- backend/app/controllers/api/v1/categories_controller.rb
+++ backend/app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::CategoriesController < ApplicationController
+   # Categories index
+   def index 
+      categories = Category.all 
+
+      render json: categories, only: [:name]
+   end 
+
+   # Show category
+   def show 
+      category = Category.find_by(id: params[:id])
+
+      render json: category, only: [:name]
+   end 
 end
