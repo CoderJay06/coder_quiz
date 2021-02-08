55491779f066211bce3efbd070b16bc68a258d3e
CoderJay06 <jaycru06@yahoo.com>
2021-02-08 02:41:41 -0500

    Implement category serializer

--- backend/app/controllers/api/v1/categories_controller.rb
+++ backend/app/controllers/api/v1/categories_controller.rb
@@ -4,17 +4,19 @@ class Api::V1::CategoriesController < ApplicationController
       categories = Category.all 
 
       # render json: categories, only: [:id, :name]
-      render json: categories.to_json(:include => {:quizzes => {
-         :only => [:id, :title, :difficultyLevel, :questionAmount]
-      }})
+      # render json: categories.to_json(:include => {:quizzes => {
+      #    :only => [:id, :title, :difficultyLevel, :questionAmount]
+      # }})
+      render json: CategorySerializer.new(categories)
    end 
 
    # Show category
    def show 
       category = Category.find_by(id: params[:id])
       # byebug
-      render json: category.to_json(:include => {:quizzes => {
-         :only => [:id, :title, :difficultyLevel, :questionAmount]
-      }})
+      # render json: category.to_json(:include => {:quizzes => {
+      #    :only => [:id, :title, :difficultyLevel, :questionAmount]
+      # }})
+      render json: CategorySerializer.new(category)
    end 
 end
