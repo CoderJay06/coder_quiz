5570fccd0cea002ec9959ca6603b13786246b823
CoderJay06 <jaycru06@yahoo.com>
2021-02-11 16:48:10 -0500

    Add category and quiz validations

--- backend/app/models/category.rb
+++ backend/app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
+   validates :name, 
+   presence: true, uniqueness: true,
+   length: { minimum: 3, maximum: 40 }  
    has_many :quizzes, dependent: :destroy
 end
