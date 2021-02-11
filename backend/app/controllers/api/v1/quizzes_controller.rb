f861739d87db02cf9c579d6b02d517bca821409e
CoderJay06 <jaycru06@yahoo.com>
2021-02-11 15:30:00 -0500

    Refactor categories and quizzes controllers

--- backend/app/controllers/api/v1/quizzes_controller.rb
+++ backend/app/controllers/api/v1/quizzes_controller.rb
@@ -2,14 +2,12 @@ class Api::V1::QuizzesController < ApplicationController
    def index
       quizzes = Quiz.all 
  
-      # render json: quizzes, except: [:created_at, :updated_at]
       render json: QuizSerializer.new(quizzes), status: :accepted
    end 
 
    def show 
       quiz = Quiz.find_by(id: params[:id])
-      # byebug
-      # render json: quiz, except: [:created_at, :updated_at]
+      
       render json: QuizSerializer.new(quiz), status: :accepted
    end 
 end
