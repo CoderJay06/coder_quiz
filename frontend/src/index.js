b62b4036ce9d4785e0cefa8fac6dd5ad54ea062b
github username <jaycru06@yahoo.com>
2021-03-10 20:39:35 -0500

    Moved categories and quizzes urls into fetch functions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 const BASE_URL = "http://localhost:3000" // Set global variable for home url
 // const USERS_URL = `${BASE_URL}/api/v1/users` // Set url global variable for users
 // const SESSIONS_URL = `${BASE_URL}/sessions` // Set url global variable for login
-const CATEGORIES_URL = `${BASE_URL}/api/v1/categories` // Set url global variable for fetching categorues
-const QUIZZES_URL = `${BASE_URL}/api/v1/quizzes`
+// const CATEGORIES_URL = `${BASE_URL}/api/v1/categories` // Set url global variable for fetching categorues
+// const QUIZZES_URL = `${BASE_URL}/api/v1/quizzes`
 const signupForm = document.querySelector("#signup-form")
 const loginForm = document.querySelector("#login-form")
 
@@ -38,6 +38,7 @@ const createCategorySelector = () => {
 }
 
 const fetchCategories = () => {
+    const CATEGORIES_URL = `${BASE_URL}/api/v1/categories`
     // Fetch and load all categories
     fetch(CATEGORIES_URL, {
             method: "GET",
@@ -90,6 +91,7 @@ const handleCategoryClick = (event) => {
 }
 
 const fetchQuiz = () => {
+    const QUIZZES_URL = `${BASE_URL}/api/v1/quizzes`
     fetch(QUIZZES_URL, {
             method: "GET",
             headers: {
